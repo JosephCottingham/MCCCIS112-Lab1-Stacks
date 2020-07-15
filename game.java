28dc6184173aa90d5f6e689c573b77f94ef0de3c
kfisher2016 <67563507+kfisher2016@users.noreply.github.com>
2020-07-15 12:28:52 -0400

    game update

--- game.java
+++ game.java
@@ -11,27 +11,26 @@ public class game
          System.out.println(b.getCurBoardAsStr());
          System.out.print("\n[m]ove, [u]ndo, [r]edo, [q]uit: ");
          menu = scn.nextLine().toCharArray()[0];
-         switch (menu[0]) {
+       
          System.out.println("\n[m]ove, [u]ndo, [r]edo, [q]uit: ");
          System.out.print("Enter Value: ");
          menu = scn.next().charAt(0);
          switch (menu) {
             case 'm':
-               System.out.print("\nY Coorinate: ");
-               String C1 = scn.next();
                System.out.print("\nX Coorinate: ");
                String C1 = scn.next();
                System.out.print("\nY Coorinate: ");
                String C2 = scn.next();
                b.move(Integer.parseInt(C1), Integer.parseInt(C2));
                break;
+              case 'u':
                b.undo();
                break;
             case 'r':
-
                b.redo();
                break;
-         }
-      } while (menu[0]!='q');
-      } while (menu!='q');
+         } 
+      }
+       while (menu!='q');
    }
+ }

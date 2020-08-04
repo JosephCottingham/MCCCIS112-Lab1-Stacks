f08e3a89493a6daf715a825d4f2bd07f271bffc3
kfisher2016 <67563507+kfisher2016@users.noreply.github.com>
2020-08-04 17:24:48 -0400

    counter class, JOptionPane split
    just made a counter class to be used to keep track of tickets left. Then just did the split of the JOptionPane input. Very choppy and have to start putting stuff together with queue, but wanted to get some of it out of the way.

--- Lab 2/QueueTicketProcessor.java
+++ Lab 2/QueueTicketProcessor.java
@@ -9,10 +9,15 @@ public class QueueTicketProcessor
       //TicketQueue tickets = new TicketQueue(100);
       //Thread test = new Thread(tickets);
       //test.start();  
+      System.out.println("Thank you for doing business with us! We look forward to helping you order your tickets for this event."); 
       System.out.println("You will be prompted to insert name and number of tickets. Enter Name:# (Ex: Katie:4)"); //instruction for user to input ticket request
       
       String ticketInput = JOptionPane.showInputDialog("Name and number of tickets: "); //JOptionPane for user to enter information
       System.out.println("Entry Confirmation: " + ticketInput); //Confirmation of what user input
+      String split[] = ticketInput.split(":"); // split of input to separate the number of tickets requested
+      
+      int ticketAmount = Integer.parseInt(split[1]); // number to be pushed onto Queue and to use to subtract from total tickets
+      System.out.println(ticketAmount); //check to see if split worked (can be taken out later)
       
       
    
--- Lab 2/TicketQueue.java
+++ Lab 2/TicketQueue.java
@@ -6,6 +6,26 @@ public class TicketQueue implements Runnable
    
    protected LLNode<Integer> TicketQueueHead = null;
    
+      
    
+
+}
+
+public class ticketCount // counter class for keeping track of tickets left after purchases
+{
+   private int ticketCounter;
+   public Counter ()
+   {
+      ticketCounter = 100;
+   }
+   public void decrement()
+   {
+      ticketCounter--;
+   }
+   
+   public int getTicketCount()
+   {
+      return ticketCounter;
+   }
 
 }
